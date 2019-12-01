7d6149efdc49178ed9de2986b1f728c94f39466a
JJINUUU <201703ska@naver.com>
2019-12-01 19:42:36 +0900

    JW branch check

--- app/src/main/java/com/example/ssuapp/MainActivity.java
+++ app/src/main/java/com/example/ssuapp/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    //Jinwoo's branch
+    //Jinwoo's branch 확인
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

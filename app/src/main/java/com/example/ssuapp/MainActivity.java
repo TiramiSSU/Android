f1f7aa704a11cca832394249b0483d56c6b89e71
JJINUUU <201703ska@naver.com>
2019-12-02 18:34:43 +0900

    프로젝트 진행도 관리 액티비티 추가

--- app/src/main/java/com/example/ssuapp/MainActivity.java
+++ app/src/main/java/com/example/ssuapp/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.ssuapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    //Jinwoo's branch 확인
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button PMbtn = (Button)findViewById(R.id.PMbtn);
+        PMbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ProjectManagementActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/ssuapp/ProjectManagementActivity.java
@@ -0,0 +1,14 @@
+package com.example.ssuapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProjectManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_management);
+    }
+}

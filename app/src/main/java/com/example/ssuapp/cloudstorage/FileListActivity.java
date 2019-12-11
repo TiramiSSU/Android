1c4953a6fc503ec31f27d6fc3932e7bd736b5c67
P-uyoung <wooyoung9654>
2019-12-11 23:19:56 +0900

    파이어베이스 스토리지 파일리스트기능 추가

---
+++ app/src/main/java/com/example/ssuapp/cloudstorage/FileListActivity.java
@@ -0,0 +1,16 @@
+package com.example.ssuapp.cloudstorage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ssuapp.R;
+
+public class FileListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_list);
+    }
+}
---
+++ app/src/main/java/com/example/ssuapp/cloudstorage/UploadInfo.java
@@ -0,0 +1,4 @@
+package com.example.ssuapp.cloudstorage;
+
+public class UploadInfo {
+}
